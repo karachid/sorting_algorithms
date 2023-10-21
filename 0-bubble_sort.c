81f26eae3a47e320071230e93f1ae382fa49436a
Rachid KABBAB <r.kabbab@gmail.com>
2023-10-21 07:58:50 -0700

    0 Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Implements bubble sort algorithm
+ * @array : Input array to be sorted
+ * @size : size of the array to be sorted
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, sorted, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		sorted = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				sorted = 1;
+				print_array(array, size);
+			}
+		}
+		if (!sorted)
+			break;
+	}
+}
