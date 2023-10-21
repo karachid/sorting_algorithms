d8edae18d70e75a28d818fbdcce10b392405bae6
Anasoud1 <anasoudsassi@gmail.com>
2023-10-21 08:31:16 -0700

    adding 2-selection_sort.c and 2-O

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort -implement selection sort algorithm
+ * @array: input array to the sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int min;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				k = j;
+			}
+		}
+		if (k != i)
+		{
+			array[k] = array[i];
+			array[i] = min;
+			print_array(array, size);
+		}
+	}
+}
