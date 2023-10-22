cb70a5a7c4a76d79c72a1f778fd0f11ba205ca8b
Anasoud1 <anasoudsassi@gmail.com>
2023-10-22 15:20:15 -0700

    adding condtion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr1, *curr2, *tmp;
 
-	if (*list == NULL)
+	if (list == NULL || *list == NULL)
 		return;
 	curr1 = (*list)->next;
 	while (curr1)
